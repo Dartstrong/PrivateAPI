7d0ce7dcf191cde277be9fd0f9e019be4850ece6
dartstrong19xx@gmail.com <dartstrong19xx@gmail.com>
2024-04-15 17:21:24 +0300

    #update entity: AsymPublickey

--- PrivateAPI/Entities/AsymPublicKey.cs
+++ PrivateAPI/Entities/AsymPublicKey.cs
@@ -7,5 +7,32 @@ namespace PrivateAPI.Entity
 {
     public class AsymPublicKey
     {
+        public string StrKey
+        {
+            get
+            {
+                return StrKey;
+            }
+            set
+            {
+                ByteArrayKey = StrToByteArray(value);
+            }
+        }
+        public byte[] ByteArrayKey { get; private set; }
+        private static byte[] StrToByteArray(string strKey)
+        {
+            string[] arrayStr = strKey.Split('-');
+            int[] arrayInt = new int[arrayStr.Length];
+            for (int i = 0; i < arrayStr.Length; i++)
+            {
+                arrayInt[i] = Convert.ToInt32(arrayStr[i]);
+            }
+            byte[] arrayByte = new byte[arrayInt.Length];
+            for (int i = 0; i < arrayByte.Length; i++)
+            {
+                arrayByte[i] = Convert.ToByte(arrayInt[i]);
+            }
+            return arrayByte;
+        }
     }
 }
